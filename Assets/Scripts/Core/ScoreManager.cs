627218379711a69e70efb23ac9a02326114a2af9
NakHyeong <snh0719@gmail.com>
2025-07-28 14:04:58 +0900

    .
    .

--- Assets/Scripts/Core/ScoreManager.cs
+++ Assets/Scripts/Core/ScoreManager.cs
@@ -21,14 +21,36 @@ public class ScoreManager : MonoBehaviour
     public void AddScore(int value)
     {
         score += value;
-        // UI 업데이트 제거
+        
     }
 
     public void ResetScore()
     {
         score = 0;
-        // UI 업데이트 제거
+        
+    }
+    public void SaveHighScore()
+    {
+        int bestScore = PlayerPrefs.GetInt("HighScore", 0);
+        if (score > bestScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            PlayerPrefs.Save();
+        }
     }
 
-    // 나머지 저장/불러오기 메서드 유지
+    public void SaveLastScore()
+    {
+        PlayerPrefs.SetInt("LastScore", score);
+        PlayerPrefs.Save();
+    }
+    public int GetHighScore()
+    {
+        return PlayerPrefs.GetInt("HighScore", 0);
+    }
+
+    public int GetLastScore()
+    {
+        return PlayerPrefs.GetInt("LastScore", 0);
+    }
 }
