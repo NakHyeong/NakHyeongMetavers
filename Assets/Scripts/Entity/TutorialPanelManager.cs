fc659445c0156e734315098da3c3f488fa6ff53c
NakHyeong <snh0719@gmail.com>
2025-07-28 15:31:57 +0900

    불친절한나켱씨
    Ui추가..

---
+++ Assets/Scripts/Entity/TutorialPanelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPanelManager : MonoBehaviour
+{
+    public GameObject tutorialPanel;
+    public Button closeButton;
+
+    void Start()
+    {
+        // 시작할 때 패널이 자동으로 보여짐
+        tutorialPanel.SetActive(true);
+        closeButton.onClick.AddListener(() =>
+        {
+            tutorialPanel.SetActive(false);
+        });
+    }
+}
